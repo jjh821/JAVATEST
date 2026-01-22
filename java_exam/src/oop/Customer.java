d27065d38d4aa8177dfa6623c247dc7a0dbe5214
jjh821 <115762980+jjh821@users.noreply.github.com>
2026-01-22 09:43:02 +0900

    Update Customer.java
    무게를 외부로 지정하기 위해 무게 상한선 int 인자값 삭제후 test 에서 지정

--- java_exam/src/oop/Customer.java
+++ java_exam/src/oop/Customer.java
@@ -4,7 +4,7 @@ public class Customer {
 	int price = 1000; // 상품 가격
 	int proWeight = 500; // 상품 무게 
 	int shopBagCount = 0; //현재 상품 개수
-	int bagWeight = 10000; // 무게 상한선
+	int bagWeight; // 무게 상한선
 	int wallte; // 지갑
 	public void Byecount(int num) {
 		if (wallte < (num * price)) {
