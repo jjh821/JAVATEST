6851b687c1a991886302273f4c99938ab1e46a4f
jjh821 <jjh8212006@gmail.com>
2026-02-03 08:59:38 +0900

    항공예약

--- article/src/com/ktdsuniversity/edu/generics/collection/list/airplanelist/AirplaneSeat.java
+++ article/src/com/ktdsuniversity/edu/generics/collection/list/airplanelist/AirplaneSeat.java
@@ -1,21 +1,34 @@
 package com.ktdsuniversity.edu.generics.collection.list.airplanelist;
 
-
-
-
 public class AirplaneSeat {
 
-	private int number;
-	private String seatname;
-	
-	public AirplaneSeat(int number, String seatname) {
-		this.number = number;
-		this.seatname = seatname;
-	}
-	
-	@Override
-	public String toString() {
-		
-		return super.toString();
-	}
+    private int number;
+    private String seatName;
+    private boolean isReserved; // 추가: 예약 여부 상태값
+
+    public AirplaneSeat(int number, String seatName) {
+        this.number = number;
+        this.seatName = seatName;
+        this.isReserved = false; // 기본값은 예약 안 됨(O)
+    }
+
+
+    public int getNumber() { 
+    	return number; 
+    	}
+    public String getSeatName() { 
+    	return seatName; 
+    	}
+    public boolean isReserved() { 
+    	return isReserved; 
+    	}
+    public void setReserved(boolean reserved) { 
+    	isReserved = reserved; 
+    	}
+
+    @Override
+    public String toString() {
+        // 예약 여부
+        return number + " : " + (isReserved ? "X" : "O");
+    }
 }
